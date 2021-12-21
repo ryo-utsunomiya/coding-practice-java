c1739df5b9ad1c7e53e0da9bec84497d279ec331
Ryo Utsunomiya <millionsnowliving@gmail.com>
2021-12-21 09:37:13 +0900

    add 2 solutions

---
+++ app/src/main/java/leetcode/reversestring/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.reversestring;
+
+public class Solution {
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        var s = new Solution();
+        var chars = new char[]{'h', 'e', 'l', 'o'};
+        s.reverseString(chars);
+        System.out.println(chars);
+    }
+}
