e3391c431662560b6f12489a4b32d5c463ddc3c8
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-01-16 13:57:30 +0900

    optimize strstr solution

--- app/src/main/java/leetcode/implementstrstr/Solution.java
+++ app/src/main/java/leetcode/implementstrstr/Solution.java
@@ -2,35 +2,7 @@ package leetcode.implementstrstr;
 
 public class Solution {
     public int strStr(String haystack, String needle) {
-        int haystackLength = haystack.length();
-        int needleLength = needle.length();
-        if (needleLength == 0) {
-            return 0;
-        }
-        if (haystackLength == 0) {
-            return -1;
-        }
-
-        char first = needle.charAt(0);
-        int max = haystackLength - needleLength;
-
-        for (int i = 0; i <= max; i++) {
-            char c = haystack.charAt(i);
-            if (c == first) {
-                int j = i + 1;
-                int end = j + needleLength - 1;
-                int k = 1;
-                while (j < end && haystack.charAt(j) == needle.charAt(k)) {
-                    j++;
-                    k++;
-                }
-                if (j == end) {
-                    return i;
-                }
-            }
-        }
-
-        return -1;
+        return haystack.indexOf(needle);
     }
 
     public static void main(String[] args) {
