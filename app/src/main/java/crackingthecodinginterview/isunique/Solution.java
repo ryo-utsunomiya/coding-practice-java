0633b02db88e225c81d180f2eddcc6f7dc5d73e4
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-07-04 23:25:10 +0900

    Improve Is Unique

--- app/src/main/java/crackingthecodinginterview/isunique/Solution.java
+++ app/src/main/java/crackingthecodinginterview/isunique/Solution.java
@@ -2,7 +2,8 @@ package crackingthecodinginterview.isunique;
 
 import java.util.HashSet;
 
-// 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters.
+// 1.1 Is Unique
+// Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structures?
 public class Solution {
     public boolean isUniqueChars(String str) {
