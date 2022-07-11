36f278a7b754c3b8931980cab9126a511da766f8
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-07-11 15:41:59 +0900

    Add LinkedList

---
+++ app/src/main/java/udemyalgo/linkedlist/LinkedList.java
@@ -0,0 +1,99 @@
+package udemyalgo.linkedlist;
+
+public class LinkedList<E> {
+    private Node<E> head;
+
+    public LinkedList() {
+        this(null);
+    }
+
+    public LinkedList(Node<E> head) {
+        this.head = head;
+    }
+
+    public void append(E data) {
+        var newNode = new Node<>(data);
+        if (this.head == null) {
+            this.head = newNode;
+            return;
+        }
+
+        var lastNode = this.head;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+    }
+
+    public void insert(E data) {
+        var newNode = new Node<>(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    public void remove(E data) {
+        var currentNode = this.head;
+        if (currentNode != null && currentNode.data == data) {
+            this.head = currentNode.next;
+            return;
+        }
+
+        Node<E> previousNode = null;
+        while (currentNode != null && currentNode.data != data) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        if (currentNode != null) {
+            previousNode.next = currentNode.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        var sb = new StringBuilder("[");
+
+        var currentNode = this.head;
+        while (true) {
+            sb.append(currentNode.data);
+            currentNode = currentNode.next;
+
+            if (currentNode == null) {
+                break;
+            } else {
+                sb.append(",");
+            }
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static class Node<E> {
+        private final E data;
+        private Node<E> next;
+
+        public Node(E data) {
+            this(data, null);
+        }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(this.data);
+        }
+    }
+
+    public static void main(String[] args) {
+        var l = new LinkedList<Integer>();
+        l.append(1);
+        l.append(2);
+        l.insert(3);
+        l.remove(1);
+        System.out.println(l);
+    }
+}
