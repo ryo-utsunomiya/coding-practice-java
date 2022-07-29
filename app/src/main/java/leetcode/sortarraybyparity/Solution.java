8205867c59f307a4667de599dcb96df528ab531a
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-07-30 00:27:03 +0900

    Refactor

--- app/src/main/java/leetcode/sortarraybyparity/Solution.java
+++ app/src/main/java/leetcode/sortarraybyparity/Solution.java
@@ -4,40 +4,23 @@ import java.util.Arrays;
 
 public class Solution {
     public int[] sortArrayByParity(int[] nums) {
-//        int[] result = new int[nums.length]; // Space: O(n)
-//        int index = 0;
-//
-//        // Time: O(n)
-//        for (int n : nums) {
-//            if (n % 2 == 0) {
-//                result[index] = n;
-//                index++;
-//            }
-//        }
-//        for (int n : nums) {
-//            if (n % 2 != 0) {
-//                result[index] = n;
-//                index++;
-//            }
-//        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 != 0) {
-                // find the event number in the array
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] % 2 == 0) {
-                        swap(nums, i, j);
-                    }
-                }
+        int[] result = new int[nums.length]; // Space: O(n)
+        int index = 0;
+
+        // Time: O(n)
+        for (int n : nums) {
+            if (n % 2 == 0) {
+                result[index] = n;
+                index++;
             }
         }
-
-        return nums;
-    }
-
-    void swap(int[] nums, int a, int b) {
-        int tmp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = tmp;
+        for (int n : nums) {
+            if (n % 2 != 0) {
+                result[index] = n;
+                index++;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
