9d218a4adcb27fd0d3c2516053b821e606cb9127
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-08-03 00:27:01 +0900

    Add Binary Tree Level Order Traversal

--- app/src/main/java/leetcode/binarytreelevelordertraversal/Solution.java
+++ app/src/main/java/leetcode/binarytreelevelordertraversal/Solution.java
@@ -2,15 +2,39 @@ package leetcode.binarytreelevelordertraversal;
 
 import leetcode.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
         if (root == null) {
-            return List.of();
+            return result;
         }
 
-        return List.of();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> numbers = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.peek();
+                assert curr != null;
+                numbers.add(curr.val);
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+                queue.poll();
+            }
+            result.add(numbers);
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
@@ -33,5 +57,14 @@ public class Solution {
 
         // []
         System.out.println(s.levelOrder(null));
+
+        // [[1],[2,3],[4,5]]
+        System.out.println(s.levelOrder(
+                new TreeNode(
+                        1,
+                        new TreeNode(2, new TreeNode(4), null),
+                        new TreeNode(3, null, new TreeNode(5))
+                )
+        ));
     }
 }
