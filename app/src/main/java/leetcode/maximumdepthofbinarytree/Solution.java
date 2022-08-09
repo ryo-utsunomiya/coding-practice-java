9eae6c1aea0d5d6a2af1589587cd9abd35b6a499
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-08-09 23:39:14 +0900

    Optimize to use iteration

--- app/src/main/java/leetcode/maximumdepthofbinarytree/Solution.java
+++ app/src/main/java/leetcode/maximumdepthofbinarytree/Solution.java
@@ -2,12 +2,39 @@ package leetcode.maximumdepthofbinarytree;
 
 import leetcode.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        Deque<Integer> depths = new ArrayDeque<>();
+        depths.push(1);
+
+        int maxDepth = 0;
+        int currentDepth;
+        TreeNode current;
+        while (!stack.isEmpty()) {
+            current = stack.pop();
+            currentDepth = depths.pop();
+            maxDepth = Math.max(maxDepth, currentDepth);
+            if (current.left != null) {
+                stack.push(current.left);
+                depths.push(currentDepth + 1);
+            }
+            if (current.right != null) {
+                stack.push(current.right);
+                depths.push(currentDepth + 1);
+            }
+        }
+
+        return maxDepth;
     }
 
     public static void main(String[] args) {
