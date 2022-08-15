edf58198de2f07b7b1da09d95440a9c549aff03c
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-08-15 21:00:49 +0900

    Refactor

--- app/src/main/java/leetcode/maximumsubarray/Solution.java
+++ app/src/main/java/leetcode/maximumsubarray/Solution.java
@@ -3,16 +3,17 @@ package leetcode.maximumsubarray;
 // https://leetcode.com/problems/maximum-subarray/
 public class Solution {
     public int maxSubArray(int[] nums) {
-        int current = nums[0];
-        int max = nums[0];
+        int currentSubArraySum = nums[0];
+        int maxSubArraySum = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
             int num = nums[i];
-            current = Math.max(num, current + num);
-            max = Math.max(max, current);
+            // when currentSubArraySum + num is smaller than num, discard currentSubArray
+            currentSubArraySum = Math.max(num, currentSubArraySum + num);
+            maxSubArraySum = Math.max(maxSubArraySum, currentSubArraySum);
         }
 
-        return max;
+        return maxSubArraySum;
     }
 
     public static void main(String[] args) {
