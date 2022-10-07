885bdc565e52fc49e7f112b7ca3139c665fb6aa5
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-10-07 23:36:49 +0900

    Refactor TreeNode

--- app/src/main/java/leetcode/TreeNode.java
+++ app/src/main/java/leetcode/TreeNode.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.LinkedList;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -13,5 +15,26 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        var queue = new LinkedList<TreeNode>();
+        queue.offer(this);
+        var values = new LinkedList<String>();
+
+        // bfs
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                values.add("null");
+            } else {
+                values.add(String.valueOf(node.val));
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+
+        return "[" + String.join(",", values) + "]";
     }
 }
