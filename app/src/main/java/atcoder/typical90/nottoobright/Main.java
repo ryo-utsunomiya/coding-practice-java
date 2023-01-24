51c57df07655efaeb31fe1008c7e3edf4f0ee019
Ryo Utsunomiya <millionsnowliving@gmail.com>
2023-01-24 19:40:35 +0900

    Add typical90

---
+++ app/src/main/java/atcoder/typical90/nottoobright/Main.java
@@ -0,0 +1,20 @@
+package atcoder.typical90.nottoobright;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+
+        if (h == 1 || w == 1) {
+            System.out.println(h * w);
+        } else {
+            System.out.println( ((h+1)/2) * ((w+1)/2) );
+        }
+
+        sc.close();
+    }
+}
