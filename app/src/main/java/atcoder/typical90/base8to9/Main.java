2c90fb963bcaab90ade97ece70993e1461a8d53a
Ryo Utsunomiya <millionsnowliving@gmail.com>
2023-03-02 23:49:55 +0900

    Add Base 8 to 9

---
+++ app/src/main/java/atcoder/typical90/base8to9/Main.java
@@ -0,0 +1,41 @@
+package atcoder.typical90.base8to9;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        String n = sc.next();
+        int k = sc.nextInt();
+        while (k > 0) {
+            String base9 = longToBase9(base8ToLong(n));
+            n = base9.replace('8', '5');
+            k--;
+        }
+        System.out.println(n);
+
+        sc.close();
+    }
+
+    static long base8ToLong(String s) {
+        long result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result = result * 8 + (s.charAt(i) - '0');
+        }
+        return result;
+    }
+
+    static String longToBase9(long n) {
+        if (n == 0) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.append((char)(n % 9 + '0'));
+            n /= 9;
+        }
+        return sb.reverse().toString();
+    }
+}
