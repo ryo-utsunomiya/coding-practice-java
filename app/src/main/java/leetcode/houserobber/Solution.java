3d1ec48b42b12b7744e006c05d0170d36cfb5d29
Ryo Utsunomiya <millionsnowliving@gmail.com>
2024-01-26 00:17:11 +0900

    Refactor

--- app/src/main/java/leetcode/houserobber/Solution.java
+++ app/src/main/java/leetcode/houserobber/Solution.java
@@ -2,20 +2,18 @@ package leetcode.houserobber;
 
 public class Solution {
     public int rob(int[] nums) {
-        int N = nums.length;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        int[] maxRobbedAmount = new int[nums.length + 1];
-        maxRobbedAmount[N] = 0;
-        maxRobbedAmount[N - 1] = nums[N - 1];
-
-        for (int i = N - 2; i >= 0; i--) {
-            maxRobbedAmount[i] = Math.max(
-                    maxRobbedAmount[i + 1],
-                    maxRobbedAmount[i + 2] + nums[i]
-            );
+        for (int i = 2; i < nums.length; i++) {
+            // ある家iにおける最大利得 dp[i] は以下のうち大きいほう
+            // 家iを盗む場合は、 nums[i] + dp[i-2] （一つ前の家は盗めないので、現在の家の金額+2つ前の家までの最大利得）
+            // 家iを盗まない場合は、 dp[i-1] （一つ前の家の最大利得を引き継ぐ）
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
         }
 
-        return maxRobbedAmount[0];
+        return dp[nums.length - 1];
     }
 
     public static void main(String[] args) {
