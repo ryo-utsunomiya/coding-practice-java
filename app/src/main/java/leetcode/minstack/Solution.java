2b87a98b23fefe53207ff7713a0ca29adf840dc0
Ryo Utsunomiya <millionsnowliving@gmail.com>
2024-02-24 21:56:37 +0900

    Fix

--- app/src/main/java/leetcode/minstack/Solution.java
+++ app/src/main/java/leetcode/minstack/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
 
         public void push(int val) {
             stack.push(val);
-            if (minStack.isEmpty() || val < minStack.peek()) {
+            if (minStack.isEmpty() || val <= minStack.peek()) {
                 minStack.push(val);
             }
         }
